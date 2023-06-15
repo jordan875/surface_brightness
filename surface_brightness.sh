925d2f605958451a455c85c7ea88202e778f79ff
jordan875 <81822456+jordan875@users.noreply.github.com>
2023-06-15 14:35:50 +0100

    Update surface_brightness.sh
    Mac like

--- surface_brightness.sh
+++ surface_brightness.sh
@@ -1,50 +1,45 @@
 #!/bin/bash
-max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-sensitivity=$((max/10))
-min=$((max/10))
-delay=6
-while [ 1 ]
-do
-updated=1
-while [ $updated -gt 0 ]
-do
-updated=0
-backlight=$(cat /sys/class/backlight/intel_backlight/brightness)
-sensor=$(cat /sys/bus/iio/devices/iio:device0/subsystem/devices/iio:device2/in_illuminance_raw)
-target=$sensor
-if [ $backlight -gt $sensor ]
-then
-if [ $(($backlight - $sensor)) -gt $sensitivity ]
-then
-updated=1
-fi
-fi
-if [ $backlight -lt $sensor ]
-then
-if [ $(($sensor - $backlight)) -gt $sensitivity ]
-then
-updated=1
-fi
-fi
-if [ $target -gt $max ]
-then
-target=$max
-fi
-if [ $target -lt 1 ]
-then
-target=$min
-fi
-if [ $updated -gt 0 ]
-then
-echo "Starting brightness: $backlight"
-echo "Ambient light: $sensor"
-echo "Adjusted brightness: $target"
-echo "-------------------------------"
-echo "Sensitivity: $sensitivity"
-echo "Min: $min Max: $max"
-echo
-echo $target > /sys/class/backlight/intel_backlight/brightness
-fi
-done
-sleep $delay
+
+LevelSteps=60
+AnimationDelay=0.016
+MaxScreenBrightness=96000
+SensorDelay=1
+
+MinimumBrightness=001
+
+SensorToDisplayScale=24
+
+while true; do
+    Light=$(cat /sys/bus/iio/devices/iio\:device0/in_illuminance_raw)
+
+    CurrentBrightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+
+    Light=$(( $Light + $MinimumBrightness ))
+
+    TempLight=$(($Light * $SensorToDisplayScale))
+
+    if [[ $TempLight -gt $MaxScreenBrightness ]]
+    then
+        NewLight=$MaxScreenBrightness
+    else
+        NewLight=$TempLight
+    fi
+
+    DiffCount=$(( ($NewLight - $CurrentBrightness)/$LevelSteps ))
+
+    for i in $(eval echo {1..$LevelSteps} )
+    do
+        if [[ $DiffCount -lt 0 ]]
+        then
+            NewBrightnessChange=$( echo "$DiffCount" | awk -F "-" {'print$2'})
+            NewBrightnessChange=$(echo $NewBrightnessChange-)
+        else
+            NewBrightnessChange=$(echo +$DiffCount)
+        fi
+
+        brightnessctl -q s $NewBrightnessChange
+        sleep $AnimationDelay
+    done
+
+    sleep $SensorDelay
 done
